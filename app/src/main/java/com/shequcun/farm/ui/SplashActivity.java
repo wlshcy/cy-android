cd08da557376612e2e660f9647bf3916de9883f3
cong <congyunlong@shequcun.com>
2015-08-20 20:37:50 +0800

    引导页面,隐藏设置图片

--- app/src/main/java/com/shequcun/farm/ui/SplashActivity.java
+++ app/src/main/java/com/shequcun/farm/ui/SplashActivity.java
@@ -1,39 +1,135 @@
 package com.shequcun.farm.ui;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.shequcun.farm.BaseFragmentActivity;
 import com.shequcun.farm.R;
+import com.shequcun.farm.data.UserLoginEntry;
+import com.shequcun.farm.datacenter.CacheManager;
 import com.shequcun.farm.datacenter.PersistanceManager;
+import com.shequcun.farm.ui.adapter.ViewPagerAdapter;
 import com.shequcun.farm.util.HttpRequestUtil;
 import com.shequcun.farm.util.LocalParams;
+
+import java.util.ArrayList;
 
 /**
  * 闪屏
  * Created by apple on 15/8/3.
  */
 public class SplashActivity extends BaseFragmentActivity {
+    private ViewPagerAdapter vpAdapter;
+    private ViewPager viewPager;
+    private ArrayList<View> views;
+    private int welcomeImg = R.drawable.splash_bg;
+    private int[] pics = {R.drawable.guide1, R.drawable.guide2,
+            R.drawable.guide3};
+    private Button mWelcomeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_ly);
-        mHandler.sendEmptyMessageDelayed(0, 1000);
         LocalParams.INSTANCE.initData(getApplicationContext());
         HttpRequestUtil.setContext(getApplicationContext());
         PersistanceManager.INSTANCE.initContext(getApplication());
+        initViewPager();
+        setOnclick();
     }
 
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            startActivity(new Intent(SplashActivity.this, SqcFarmActivity.class));
-            finish();
+            gotoHome();
         }
     };
 
+    private void gotoHome(){
+        startActivity(new Intent(SplashActivity.this, SqcFarmActivity.class));
+        finish();
+    }
+
+    private void initViewPager() {
+        mWelcomeBtn = (Button) findViewById(R.id.welcome_btn);
+        viewPager = (ViewPager) findViewById(R.id.welcome_viewpager);
+        views = new ArrayList<View>();
+//        欢迎页面
+        if (isStartUped()) {
+            ImageView iv = new ImageView(this);
+            LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT);
+            iv.setLayoutParams(mParams);
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+                iv.setBackground(getResources().getDrawable(welcomeImg));
+            } else {
+                iv.setBackgroundDrawable(getResources().getDrawable(welcomeImg));
+            }
+            views.add(iv);
+            mHandler.sendEmptyMessageDelayed(0, 1000);
+//            滑动轮播图
+        } else {
+            for (int i = 0; i < pics.length; i++) {
+                ImageView iv = new ImageView(this);
+                LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                iv.setLayoutParams(mParams);
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+                    iv.setBackground(getResources().getDrawable(pics[i]));
+                } else {
+                    iv.setBackgroundDrawable(getResources().getDrawable(pics[i]));
+                }
+                views.add(iv);
+            }
+        }
+        vpAdapter = new ViewPagerAdapter(views);
+        viewPager.setAdapter(vpAdapter);
+    }
+
+    private boolean isStartUped() {
+        UserLoginEntry userLoginEntry = new CacheManager(this).getUserLoginEntry();
+        return userLoginEntry!=null;
+    }
+
+    private void setOnclick() {
+        mWelcomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoHome();
+            }
+        });
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if (i == 2) {
+                    mWelcomeBtn.setVisibility(View.VISIBLE);
+                } else {
+                    mWelcomeBtn.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
 
 }
