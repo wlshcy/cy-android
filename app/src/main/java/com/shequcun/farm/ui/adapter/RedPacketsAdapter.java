b6e1a8d69bfa142b90a57b2092e86dd3e3dae68b
cong <congyunlong@shequcun.com>
2015-09-15 18:38:20 +0800

    添加红包类型和作用

--- app/src/main/java/com/shequcun/farm/ui/adapter/RedPacketsAdapter.java
+++ app/src/main/java/com/shequcun/farm/ui/adapter/RedPacketsAdapter.java
@@ -18,7 +18,7 @@ import java.util.List;
 /**
  * Created by cong on 15/9/7.
  */
-public class RedPacketsAdapter extends BaseAdapter{
+public class RedPacketsAdapter extends BaseAdapter {
     private List<CouponEntry> list = new ArrayList<>();
     private Context context;
     private long serveTime;
@@ -45,54 +45,66 @@ public class RedPacketsAdapter extends BaseAdapter{
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder vh;
-        if (convertView==null){
-            convertView = LayoutInflater.from(context).inflate(R.layout.item_red_packets,null);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_red_packets, null);
             vh = new ViewHolder();
-            vh.count = (TextView)convertView.findViewById(R.id.money_count_tv);
-            vh.expiryDate = (TextView)convertView.findViewById(R.id.expiry_date_tv);
-            vh.moneySymbolTv = (TextView)convertView.findViewById(R.id.money_symbol_tv);
-            vh.zheTv = (TextView)convertView.findViewById(R.id.zhe_tv);
-            vh.flowerIv = (ImageView)convertView.findViewById(R.id.flower_iv);
+            vh.count = (TextView) convertView.findViewById(R.id.money_count_tv);
+            vh.expiryDate = (TextView) convertView.findViewById(R.id.expiry_date_tv);
+            vh.moneySymbolTv = (TextView) convertView.findViewById(R.id.money_symbol_tv);
+            vh.zheTv = (TextView) convertView.findViewById(R.id.zhe_tv);
+            vh.nameTv = (TextView) convertView.findViewById(R.id.red_packets_name_tv);
+            vh.targetTv = (TextView) convertView.findViewById(R.id.target_tv);
+            vh.flowerIv = (ImageView) convertView.findViewById(R.id.flower_iv);
             convertView.setTag(vh);
-        }else {
-            vh = (ViewHolder)convertView.getTag();
+        } else {
+            vh = (ViewHolder) convertView.getTag();
         }
-        CouponEntry entry = (CouponEntry)getItem(position);
-        if (entry.distype==1){
-            vh.count.setText(entry.discount/100+"");
+        CouponEntry entry = (CouponEntry) getItem(position);
+        if (entry.distype == 1) {
+            vh.count.setText(entry.discount / 100 + "");
             vh.zheTv.setVisibility(View.GONE);
-        }else if (entry.distype==2){
-            vh.count.setText(((float)entry.discount)/10+"");
+        } else if (entry.distype == 2) {
+            vh.count.setText(((float) entry.discount) / 10 + "");
             vh.moneySymbolTv.setVisibility(View.GONE);
         }
-        if (entry.used||(serveTime>0&&entry.expire<=serveTime)){
+        if (entry.type == 1) {
+            vh.nameTv.setText("宅配套餐优惠红包");
+            vh.nameTv.setText("仅用于购买宅配套餐");
+        } else {
+            vh.nameTv.setText("商品优惠红包");
+            vh.nameTv.setText("仅用于农庄优选商品");
+        }
+
+        if (entry.used || (serveTime > 0 && entry.expire <= serveTime)) {
             vh.count.setTextColor(context.getResources().getColor(R.color.gray_cccccc));
             vh.moneySymbolTv.setTextColor(context.getResources().getColor(R.color.gray_cccccc));
             vh.zheTv.setTextColor(context.getResources().getColor(R.color.gray_cccccc));
             vh.expiryDate.setTextColor(context.getResources().getColor(R.color.gray_cccccc));
             vh.flowerIv.setBackgroundResource(R.drawable.flower_stroke_gray);
-        }else {
+        } else {
             vh.flowerIv.setBackgroundResource(R.drawable.flower_stroke);
         }
-        vh.expiryDate.setText("有效期至"+Utils.getTime(entry.expire));
+        vh.expiryDate.setText("有效期至" + Utils.getTime(entry.expire));
         return convertView;
     }
 
-    public void addAll(List<CouponEntry> list){
+    public void addAll(List<CouponEntry> list) {
         this.list.addAll(list);
         notifyDataSetChanged();
     }
 
-    public Object getLastItem(){
-        if (this.list.isEmpty())return null;
-        return getItem(this.getCount()-1);
+    public Object getLastItem() {
+        if (this.list.isEmpty()) return null;
+        return getItem(this.getCount() - 1);
     }
 
-    static class ViewHolder{
+    static class ViewHolder {
         TextView count;
         TextView expiryDate;
         TextView moneySymbolTv;
         TextView zheTv;
+        TextView nameTv;
+        TextView targetTv;
         ImageView flowerIv;
     }
 
