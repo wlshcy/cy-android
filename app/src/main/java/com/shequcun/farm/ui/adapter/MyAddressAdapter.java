0dace33a242c7e4dc01b6a7ea8350f89c1f2f409
cong <congyunlong@shequcun.com>
2015-09-16 14:12:03 +0800

    修改我的地址新提的问题

--- app/src/main/java/com/shequcun/farm/ui/adapter/MyAddressAdapter.java
+++ app/src/main/java/com/shequcun/farm/ui/adapter/MyAddressAdapter.java
@@ -2,6 +2,7 @@ package com.shequcun.farm.ui.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -20,6 +21,9 @@ import java.util.ArrayList;
 public class MyAddressAdapter extends BaseAdapter {
     private ArrayList<AddressEntry> list = new ArrayList<AddressEntry>();
     private Context context;
+    private boolean showDefaultIcon;
+    private OnUpdateAddressListener onUpdateAddressListener;
+    private OnChooseAddressListener onChooseAddressListener;
 
     public MyAddressAdapter(Context context) {
         this.context = context;
@@ -49,6 +53,8 @@ public class MyAddressAdapter extends BaseAdapter {
             vh.name = (TextView) convertView.findViewById(R.id.name_tv);
             vh.mobile = (TextView) convertView.findViewById(R.id.mobile_tv);
             vh.address = (TextView) convertView.findViewById(R.id.address_tv);
+            vh.update = (TextView) convertView.findViewById(R.id.update_address_tv);
+            vh.addressRl = convertView.findViewById(R.id.address_rl);
             vh.choose = (ImageView) convertView.findViewById(R.id.choose_iv);
             convertView.setTag(vh);
         } else {
@@ -57,9 +63,30 @@ public class MyAddressAdapter extends BaseAdapter {
         AddressEntry entry = (AddressEntry) getItem(position);
         vh.name.setText(entry.name);
         vh.mobile.setText(entry.mobile);
-        vh.address.setText(entry.city+entry.region+entry.zname + entry.bur);
-        if (entry.isDefault)
-            vh.choose.setVisibility(View.VISIBLE);
+        vh.address.setText(entry.city + entry.region + entry.zname + entry.bur);
+        if (entry.isDefault && showDefaultIcon) {
+            vh.choose.setImageResource(R.drawable.icon_choose);
+        }else {
+            if (!showDefaultIcon){
+                vh.choose.setVisibility(View.GONE);
+            }
+        }
+        vh.update.setTag(entry);
+        vh.update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onUpdateAddressListener != null)
+                    onUpdateAddressListener.onUpdate((AddressEntry) v.getTag());
+            }
+        });
+        vh.addressRl.setTag(entry);
+        vh.addressRl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onChooseAddressListener != null)
+                    onChooseAddressListener.onChoose((AddressEntry) v.getTag());
+            }
+        });
         return convertView;
     }
 
@@ -68,12 +95,12 @@ public class MyAddressAdapter extends BaseAdapter {
         notifyDataSetChanged();
     }
 
-    public void clear(){
+    public void clear() {
         this.list.clear();
         notifyDataSetChanged();
     }
 
-    public void remove(int pos){
+    public void remove(int pos) {
         this.list.remove(pos);
         notifyDataSetChanged();
     }
@@ -82,6 +109,28 @@ public class MyAddressAdapter extends BaseAdapter {
         TextView name;
         TextView mobile;
         TextView address;
+        TextView update;
+        View addressRl;
         ImageView choose;
+    }
+
+    public void setShowDefaultIcon(boolean showDefaultIcon) {
+        this.showDefaultIcon = showDefaultIcon;
+    }
+
+    public interface OnUpdateAddressListener {
+        void onUpdate(AddressEntry entry);
+    }
+
+    public interface OnChooseAddressListener {
+        void onChoose(AddressEntry entry);
+    }
+
+    public void setOnUpdateAddressListener(OnUpdateAddressListener onUpdateAddressListener) {
+        this.onUpdateAddressListener = onUpdateAddressListener;
+    }
+
+    public void setOnChooseAddressListener(OnChooseAddressListener onChooseAddressListener) {
+        this.onChooseAddressListener = onChooseAddressListener;
     }
 }
