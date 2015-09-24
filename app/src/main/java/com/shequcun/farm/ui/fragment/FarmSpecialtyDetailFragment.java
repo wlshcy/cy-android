ff882bb6c6a3c4cbcb60edc2f5d8f1f3019374dd
cong <congyunlong@shequcun.com>
2015-09-24 10:07:31 +0800

    修改特产详情产品描述字体颜色

--- app/src/main/java/com/shequcun/farm/ui/fragment/FarmSpecialtyDetailFragment.java
+++ app/src/main/java/com/shequcun/farm/ui/fragment/FarmSpecialtyDetailFragment.java
@@ -2,6 +2,7 @@ package com.shequcun.farm.ui.fragment;
 
 import android.app.AlertDialog;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
