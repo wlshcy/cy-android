c65527ee8224169cdff92b867a820c210c8e324c
gongbingchuan <gongbingchuan@shequcun.com>
2015-09-24 14:02:34 +0800

    更改为正式资源

--- app/src/main/java/com/shequcun/farm/util/LocalParams.java
+++ app/src/main/java/com/shequcun/farm/util/LocalParams.java
@@ -64,9 +64,9 @@ public class LocalParams {
 
     public static String getBaseUrl() {
 //        return getKeyValue("base_url");
-//        return "https://api.shequcun.com/";
+        return "https://api.shequcun.com/";
 //        return "http://192.168.1.222:8002/";
-        return "http://192.168.1.100:9100/";
+//        return "http://192.168.1.100:9100/";
     }
 
     public static String getWxAppId() {
