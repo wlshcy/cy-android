ecdbd9142a1488b7a3059f1310b9ee6f8c8eb9aa
gongbingchuan <gongbingchuan@shequcun.com>
2015-10-30 11:50:08 +0800

    修复从“我的订单”页面返回“我的”页面，“我的订单”页面显示瑕疵的问题

--- app/src/main/java/com/shequcun/farm/ui/fragment/MyComboOrderViewHolder.java
+++ app/src/main/java/com/shequcun/farm/ui/fragment/MyComboOrderViewHolder.java
@@ -1,10 +1,7 @@
 package com.shequcun.farm.ui.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -12,10 +9,8 @@ import com.common.widget.PullToRefreshBase;
 import com.common.widget.PullToRefreshScrollView;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.shequcun.farm.R;
-import com.shequcun.farm.data.HistoryOrderEntry;
 import com.shequcun.farm.data.MyComboOrder;
 import com.shequcun.farm.data.MyComboOrderListEntry;
-import com.shequcun.farm.dlg.ProgressDlg;
 import com.shequcun.farm.ui.adapter.MyComboOrderAdapter;
 import com.shequcun.farm.util.HttpRequestUtil;
 import com.shequcun.farm.util.JsonUtilsParser;
@@ -23,43 +18,59 @@ import com.shequcun.farm.util.LocalParams;
 import com.shequcun.farm.util.ToastHelper;
 import com.shequcun.farm.util.Utils;
 
-
 import java.util.List;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 import cz.msebera.android.httpclient.Header;
 
 /**
- * 我的订单——菜品订单
- * Created by mac on 15/10/8.
+ * Created by mac on 15/10/30.
  */
-public class MyComboOrderFragment extends BaseFragment {
+public class MyComboOrderViewHolder {
+    BaseFragment fgt;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.my_order_listview_ly, container, false);
-    }
-
-    @Override
-    protected void setWidgetLsn() {
+    MyComboOrderViewHolder(BaseFragment fgt, View view) {
+        this.fgt = fgt;
+        ButterKnife.bind(this, view);
+        buildAdapter();
         scroll_view.setMode(PullToRefreshBase.Mode.DISABLED);
         requestOrderNo();
     }
 
-    @Override
-    protected void initWidget(View v) {
-        buildAdapter();
+
+    void buildAdapter() {
+        if (adapter == null)
+            adapter = new MyComboOrderAdapter(fgt.getBaseAct());
+        mLv.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onBackPressed() {
-        return false;
+    @OnItemClick(R.id.mLv)
+    void onItemClick(int position) {
+        if (adapter == null || fgt == null)
+            return;
+        MyComboOrder entry = adapter.getItem(position);
+//        HistoryOrderEntry entry = adapter.getItem(position);
+//        if (entry != null) {
+//            if (entry.status == 1 || entry.status == 3 || entry.status == 0 || entry.status == 2) {
+//                gotoFragmentByAdd(buildBundle(buildOrderParams(entry)), R.id.mainpage_ly, new ModifyOrderFragment(), ModifyOrderFragment.class.getName());
+//            } else if (entry.status == 4) {
+//                ToastHelper.showShort(getBaseAct(), "您的订单已取消!");
+//            }
+//        }
+        if (entry != null) {
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("MyComboOrderEntry", entry);
+            fgt.gotoFragmentByAdd(bundle, R.id.mainpage_ly, new DishesFragment(), DishesFragment.class.getName());
+        }
     }
+
 
     void requestOrderNo() {
-        HttpRequestUtil.getHttpClient(getBaseAct()).get(LocalParams.getBaseUrl() + "cai/mycombo", new AsyncHttpResponseHandler() {
+        if (fgt == null)
+            return;
+        HttpRequestUtil.getHttpClient(fgt.getBaseAct()).get(LocalParams.getBaseUrl() + "cai/mycombo", new AsyncHttpResponseHandler() {
 
             @Override
             public void onFinish() {
@@ -82,21 +93,14 @@ public class MyComboOrderFragment extends BaseFragment {
             @Override
             public void onFailure(int sCode, Header[] h, byte[] data, Throwable error) {
                 if (sCode == 0) {
-                    ToastHelper.showShort(getBaseAct(), R.string.network_error_tip);
+                    ToastHelper.showShort(fgt.getBaseAct(), R.string.network_error_tip);
                     return;
                 }
 
-                ToastHelper.showShort(getBaseAct(), "请求失败,错误码" + sCode);
+                ToastHelper.showShort(fgt.getBaseAct(), "请求失败,错误码" + sCode);
             }
         });
     }
-
-    void buildAdapter() {
-        if (adapter == null)
-            adapter = new MyComboOrderAdapter(getBaseAct());
-        mLv.setAdapter(adapter);
-    }
-
 
     void doAddDataToAdapter(List<MyComboOrder> aList) {
         if (adapter != null) {
@@ -104,26 +108,6 @@ public class MyComboOrderFragment extends BaseFragment {
             adapter.notifyDataSetChanged();
         }
         Utils.setListViewHeightBasedOnChildren(mLv);
-    }
-
-    @OnItemClick(R.id.mLv)
-    void onItemClick(int position) {
-        if (adapter == null)
-            return;
-        MyComboOrder entry = adapter.getItem(position);
-//        HistoryOrderEntry entry = adapter.getItem(position);
-//        if (entry != null) {
-//            if (entry.status == 1 || entry.status == 3 || entry.status == 0 || entry.status == 2) {
-//                gotoFragmentByAdd(buildBundle(buildOrderParams(entry)), R.id.mainpage_ly, new ModifyOrderFragment(), ModifyOrderFragment.class.getName());
-//            } else if (entry.status == 4) {
-//                ToastHelper.showShort(getBaseAct(), "您的订单已取消!");
-//            }
-//        }
-        if (entry != null) {
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("MyComboOrderEntry", entry);
-            gotoFragmentByAdd(bundle, R.id.mainpage_ly, new DishesFragment(), DishesFragment.class.getName());
-        }
     }
 
     @Bind(R.id.progress_bar)
